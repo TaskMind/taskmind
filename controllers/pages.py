a70ab3592ce526bd174aa78c2c045ec4c29b1043
Luis Eduardo Ocegueda Cortes <luiseduardoocegueda@gmail.com>
2025-02-02 13:40:56 -0600

    feat: finish home page

--- controllers/pages.py
+++ controllers/pages.py
@@ -32,4 +32,40 @@ class PagesContoller:
           }
         ]
 
-        return render_template("index.html", characteristics=characteristics)
+        aboutUs = [
+            {
+              "title": "Nacido de la Necesidad",
+              "desc": "TaskMind surgió de nuestra propia experiencia como estudiantes, al no encontrar una plataforma simple y efectiva para gestionar proyectos grupales académicos.",
+              "icon": "fa-solid fa-graduation-cap"
+            },
+            {
+              "title": "Potenciado por lA",
+              "desc": "Integramos tecnologia de inteligencia artificial para ayudarte a organizar y priorizar tareas de manera más inteligente, adaptándose a tu forma de trabajar.",
+              "icon": "fa-solid fa-brain"
+            },
+            {
+              "title": "Open Source",
+              "desc": "Creemos en la transparencia y la colaboración. Por eso, TaskMind es completamente open source y está abierto a contribuciones de la comunidad.",
+              "icon": "fa-solid fa-code"
+            },
+        ]
+
+        contribute = [
+            {
+              "title": "Desarrollo",
+              "desc": "Ayúdanos a mejorar el código, agregar nuevas funcionalidades o corregir errores.",
+              "icon": "fa-solid fa-code"
+            },
+            {
+              "title": "Feedback",
+              "desc": "Comparte tus ideas y sugerencias para hacer TaskMind aún mejor.",
+              "icon": "fa-solid fa-comments"
+            },
+            {
+              "title": "Difusión",
+              "desc": "Ayúdanos a llegar a más estudiantes compartiendo el proyecto.",
+              "icon": "fa-solid fa-heart"
+            },
+        ]
+
+        return render_template("index.html", characteristics=characteristics, aboutUs=aboutUs, contribute=contribute)
